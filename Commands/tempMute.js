b2b37fe3c13c4e795c807f9667dafb3dfc9cbf2d
Knightplayzz <73634415+Knightplayzz@users.noreply.github.com>
2020-11-17 20:15:41 +0100

    Update tempMute.js

--- Commands/tempMute.js
+++ Commands/tempMute.js
@@ -22,6 +22,15 @@ var botEmbedError = new discord.MessageEmbed()
 
 module.exports.run = async (client, message, args) => {
 
+    var logChannel = message.guild.channels.cache.find(channel => channel.name === "log")
+
+    var warn = new discord.MessageEmbed()
+    .setTitle("**Mute**")
+    .setColor("RED")
+    .setFooter(`© created by philippe#0354`)
+    .setTimestamp()
+    .setDescription(`${mutePerson} is been muted. \n **Time** ${muteTime} \n **Muded by:** ${message.author}.`);
+
    if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send(botEmbedError);
    
    if (!args[0]) return message.channel.send(botEmbedError);
@@ -52,6 +61,8 @@ module.exports.run = async (client, message, args) => {
 
 ])
 .setFooter(`© created by philippe#0354`)
+
+logChannel.send(warn)
 
 var botEmbedUnmute = new discord.MessageEmbed()
 .setDescription("**You are unmuted!**")
