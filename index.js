c2bec0f02e1567f0bf8c801050324ec8ce458ba9
Knightplayzz <73634415+Knightplayzz@users.noreply.github.com>
2020-11-18 21:02:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const discord = require("discord.js");
 const botConfig = require("./botConfig.json");
+const welcome = require('./welcome.js')
 const fs = require ("fs");
+
+welcome(client)
 
 const bot = new discord.Client();
 bot.login(process.env.token);
 
-welcome(client)
+
 
 const activeSongs = new Map();
 
