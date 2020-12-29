43358dea8f5ef3ffde0c2a2938b070b57541ef74
Knightplayzz <73634415+Knightplayzz@users.noreply.github.com>
2020-12-29 20:55:24 +0100

    Update giveaway.js

--- Commands/giveaway.js
+++ Commands/giveaway.js
@@ -91,5 +91,6 @@ module.exports.run = async (client, message, args) => {
 module.exports.help = {
     name: "giveaway",
     description: "Geeft al de verschillende commands",
-    category: "Informatie"
+    category: "Informatie",
+    aliases: []
 }
